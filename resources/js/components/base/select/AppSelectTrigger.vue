cede690a634b6c134f5aa0acc9d7d04a04c0b38a
Mazen Touati <mazen_touati@hotmail.com>
2026-01-12 19:48:20 +0100

    chore: don't show a ring over dropdowns

--- resources/js/components/base/select/AppSelectTrigger.vue
+++ resources/js/components/base/select/AppSelectTrigger.vue
@@ -25,7 +25,7 @@ const forwardedProps = useForwardProps(delegatedProps);
         v-bind="forwardedProps"
         :class="
             cn(
-                'flex h-9 items-center justify-between rounded-md border border-zinc-200 bg-transparent px-3 py-2 text-start text-sm whitespace-nowrap shadow-sm ring-offset-white focus-visible:ring-1 focus-visible:ring-zinc-950 focus-visible:outline-none disabled:cursor-not-allowed disabled:opacity-50 data-[placeholder]:text-zinc-500 dark:border-zinc-800 dark:ring-offset-zinc-950 dark:focus-visible:ring-zinc-300 dark:data-[placeholder]:text-zinc-400 [&>span]:truncate',
+                'flex h-9 items-center justify-between rounded-md border border-zinc-200 bg-transparent px-3 py-2 text-start text-sm whitespace-nowrap shadow-sm ring-offset-white focus-visible:outline-none disabled:cursor-not-allowed disabled:opacity-50 data-[placeholder]:text-zinc-500 dark:border-zinc-800 dark:data-[placeholder]:text-zinc-400 [&>span]:truncate',
                 props.class,
             )
         "
