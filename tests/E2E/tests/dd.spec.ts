1e9cc89df2655225b9096c3608f9f1744e9b93c0
Mazen Touati <mazen_touati@hotmail.com>
2026-01-25 20:51:58 +0100

    test: update snapshot

--- tests/E2E/tests/dd.spec.ts
+++ tests/E2E/tests/dd.spec.ts
@@ -114,7 +114,7 @@ test("Dump and Die visualization sanity checklist", async ({ page }) => {
         .click();
 
     await expect(
-        page.locator("#reka-collapsible-content-v-127"),
+        page.locator("#reka-collapsible-content-v-131"),
     ).toMatchAriaSnapshot(`- text: "0: \\"/\\" (1) 1: \\"\\\\\\" (1)"`);
 
     //  dump #3 (runtime object)
