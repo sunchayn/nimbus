0c1aa82cb70c4299484cac4d5f4b58ba64d70dc7
Mazen Touati <mazen_touati@hotmail.com>
2026-01-26 00:55:56 +0100

    chore: clearer access to implementation

--- src/NimbusServiceProvider.php
+++ src/NimbusServiceProvider.php
@@ -58,7 +58,7 @@ class NimbusServiceProvider extends PackageServiceProvider
         $this->tagAlongsideLaravelAssets();
 
         // Handle transaction mode for requests
-        $this->app['events']->listen(RouteMatched::class, function (RouteMatched $event) {
+        $this->app->get('events')->listen(RouteMatched::class, function (RouteMatched $event) {
             if ($event->request->header('X-Nimbus-Transaction-Mode') === '1') {
                 app('db')->beginTransaction();
 
