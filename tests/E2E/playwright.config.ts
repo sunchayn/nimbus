f80c57d6414aff1c72c3a0a566f75f5fae72b062
Mazen Touati <mazen_touati@hotmail.com>
2026-01-30 01:56:37 +0100

    build: reconfigure PW once again

--- tests/E2E/playwright.config.ts
+++ tests/E2E/playwright.config.ts
@@ -14,12 +14,11 @@ import { defineConfig, devices } from '@playwright/test';
 export default defineConfig({
     testDir: "./tests",
     timeout: 160_000,
-    /* Run tests in files in parallel */
-    fullyParallel: true,
+    fullyParallel: false,
     /* Fail the build on CI if you accidentally left test.only in the source code. */
     forbidOnly: !!process.env.CI,
-    retries: 0,
-    workers: process.env.CI ? 1 : 8,
+    retries: 1,
+    workers: 8,
     /* Reporter to use. See https://playwright.dev/docs/test-reporters */
     reporter: "html",
     /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
