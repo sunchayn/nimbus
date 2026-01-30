21e763ab59c71874e52ec21ef22c6551e5648a39
Mazen Touati <mazen_touati@hotmail.com>
2026-01-30 02:06:20 +0100

    build: PW again

--- tests/E2E/playwright.config.ts
+++ tests/E2E/playwright.config.ts
@@ -18,7 +18,7 @@ export default defineConfig({
     /* Fail the build on CI if you accidentally left test.only in the source code. */
     forbidOnly: !!process.env.CI,
     retries: 1,
-    workers: 8,
+    workers: process.env.CI ? 4 : 8,
     /* Reporter to use. See https://playwright.dev/docs/test-reporters */
     reporter: "html",
     /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
