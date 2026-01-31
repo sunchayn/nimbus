f576895bd6971746aefe5d3679cc7983a73b5053
Mazen Touati <mazen_touati@hotmail.com>
2026-01-31 02:07:41 +0100

    test: pw one worker no retry

--- tests/E2E/playwright.config.ts
+++ tests/E2E/playwright.config.ts
@@ -17,8 +17,8 @@ export default defineConfig({
     fullyParallel: true,
     /* Fail the build on CI if you accidentally left test.only in the source code. */
     forbidOnly: !!process.env.CI,
-    retries: 1,
-    workers: 4,
+    retries: 0,
+    workers: process.env.CI ? 1 : 8,
     /* Reporter to use. See https://playwright.dev/docs/test-reporters */
     reporter: "html",
     /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
