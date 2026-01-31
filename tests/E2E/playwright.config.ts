4fbcc986fa05e829a747767fd96c0aec806866a9
Mazen Touati <mazen_touati@hotmail.com>
2026-01-31 01:36:47 +0100

    build: more PW

--- tests/E2E/playwright.config.ts
+++ tests/E2E/playwright.config.ts
@@ -18,7 +18,7 @@ export default defineConfig({
     /* Fail the build on CI if you accidentally left test.only in the source code. */
     forbidOnly: !!process.env.CI,
     retries: 1,
-    workers: 8,
+    workers: process.env.CI ? 4 : 8,
     /* Reporter to use. See https://playwright.dev/docs/test-reporters */
     reporter: "html",
     /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
@@ -33,15 +33,22 @@ export default defineConfig({
     },
 
     /* Configure projects for major browsers */
-    projects: [
-        {
-            name: "chromium",
-            use: { ...devices["Desktop Chrome"] },
-        },
+    projects: !process.env.CI
+        ? [
+            {
+                name: "chromium",
+                use: { ...devices["Desktop Chrome"] },
+            },
 
-        {
-            name: "firefox",
-            use: { ...devices["Desktop Firefox"] },
-        },
-    ],
+            {
+                name: "firefox",
+                use: { ...devices["Desktop Firefox"] },
+            },
+        ]
+        : [
+            {
+                name: "chromium",
+                use: { ...devices["Desktop Chrome"] },
+            },
+        ],
 });
