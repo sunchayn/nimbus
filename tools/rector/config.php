7938273c3eefb59ca932c8da08fda926df10218b
Mazen Touati <mazen_touati@hotmail.com>
2026-07-14 02:14:51 +0200

    chore: ignore rector for file

--- tools/rector/config.php
+++ tools/rector/config.php
@@ -29,4 +29,11 @@ return static function (RectorConfig $rectorConfig): void {
         SetList::PRIVATIZATION,
         SetList::TYPE_DECLARATION,
     ]);
+
+    $rectorConfig->skip([
+        \Rector\CodeQuality\Rector\Class_\CompleteDynamicPropertiesRector::class => [
+            __DIR__.'/../../src/Modules/Relay/Responses/DumpAndDieResponse.php', // <- has a breaking change BC fix.
+        ],
+    ]);
+
 };
